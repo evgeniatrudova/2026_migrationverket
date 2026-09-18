2ca0f7cca5bc0e2e48cbf63df4f3a948317a1bae
evgeniatrudova <68112616+evgeniatrudova@users.noreply.github.com>
2026-09-18 20:40:10 +0200

    Update page title and configuration for LGBTQ US

--- lgbtq.py
+++ lgbtq.py
@@ -24,7 +24,7 @@ except ImportError:
 # ---------------------------------------------------------
 # Configuration & Bilingual Dictionary
 # ---------------------------------------------------------
-st.set_page_config(page_title="Lifos COI: QoL & Kumulativ Bedömning", layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="LGBTQ US", layout="wide", initial_sidebar_state="collapsed")
 
 I18N = {
     "sv": {

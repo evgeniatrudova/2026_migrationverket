fd358235a1e4566bacda042e3c78bfd0d6638be9
evgeniatrudova <68112616+evgeniatrudova@users.noreply.github.com>
2026-09-18 19:17:09 +0200

    Update lgbtq.py

--- lgbtq.py
+++ lgbtq.py
@@ -21,7 +21,7 @@ st.set_page_config(page_title="LQBTQ-EVelution", layout="wide", initial_sidebar_
 I18N = {
     "sv": {
         "title": "Landinformation (COI) - Beslutsunderlag",
-        "legal_warning": "RÄTTSLIGT MEDDELANDE: AI-algoritmen är juridiskt ägd av EVelutionAB och får inte användas av Migrationsverket utan ett giltigt nyttjandeavtal.",
+        "legal_warning": "RÄTTSLIGT MEDDELANDE: AI-algoritmen är juridiskt ägd av EVelutionAB och får inte användas av Migrationsverket utan ett giltigt nyttjandeavtal. Tillfällig användning gäller vid Nathaniel Christian Karlsson bruk samt vid utvärdering av Robin L'fira ärande.",
         "admin_header": "1. Ärendeuppgifter (För närvarande inaktiverad)",
         "case_num": "Ärendenummer",
         "officer_1": "Handläggare (Signatur)",
